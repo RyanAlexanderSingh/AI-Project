48ead789b9242bb349dd8fc6ac7262cf4fe30f6a
Ryan Singh <ryan2229@hotmail.co.uk>
2015-03-04 08:22:16 +0000

    fixed orientation and size of test_ship.dae

--- octet/src/examples/Ghost/ghost.h
+++ octet/src/examples/Ghost/ghost.h
@@ -19,50 +19,67 @@ namespace octet {
     mouse_look mouse_look_helper;
     helper_fps_controller fps_helper;
 
+    ref<scene_node> player_node;
+
+    //camera instance
     ref<camera_instance> the_camera;
+
+    collada_builder loader;
 
     // scene for drawing box
     ref<visual_scene> app_scene;
-    btDefaultCollisionConfiguration config; /// setup for the world
-    btCollisionDispatcher *dispatcher; /// handler for collisions between objects
-    btDbvtBroadphase *broadphase; /// handler for broadphase (rough) collision
-    btSequentialImpulseConstraintSolver *solver; /// handler to resolve collisions
-    btDiscreteDynamicsWorld *world; /// physics world, contains rigid bodies
 
     vec2 camAngle = (0.0f, 0.0f);
 
   public:
     /// this is called when we construct the class before everything is initialised.
     ghost(int argc, char **argv) : app(argc, argv) {
-      dispatcher = new btCollisionDispatcher(&config);
-      broadphase = new btDbvtBroadphase();
-      solver = new btSequentialImpulseConstraintSolver();
-      world = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, &config);
     }
     ~ghost(){
-      delete world;
-      delete solver;
-      delete broadphase;
-      delete dispatcher;
     }
 
     /// this is called once OpenGL is initialized
     void app_init() {
 
+
       //hide the cursor
       ShowCursor(false);
       mouse_look_helper.init(this, 200.0f / 360.0f, false);
+      fps_helper.init(this);
       //set gravity for the world
-      world->setGravity(btVector3(0.0f, 0.0f, 0.0f));
+      //world->setGravity(btVector3(0.0f, 0.0f, 0.0f));
 
-      app_scene =  new visual_scene();
+      app_scene = new visual_scene();
+
+      resource_dict dict;
+      if (!loader.load_xml("assets/test_ship.dae")) {
+        // failed to load file
+        return;
+      }
+      loader.get_resources(dict);
+
+      dynarray<resource*> meshes;
+      dict.find_all(meshes, atom_mesh);
+
+      if (meshes.size()) {
+        material *mat = new material(new image("assets/test_ship.jpg"));
+        //material *mat = new material(vec4(0, 0, 0, 1));
+        mesh *player_ship = meshes[0]->get_mesh();
+        scene_node *node = new scene_node();
+        node->translate(vec3(0, 500, 0));
+        app_scene->add_child(node);
+        app_scene->add_mesh_instance(new mesh_instance(node, player_ship, mat));
+        //player_node = app_scene->get_mesh_instance(0)->get_node();
+        player_node = node;
+      }
+
       app_scene->create_default_camera_and_lights();
       the_camera = app_scene->get_camera_instance(0);
       the_camera->get_node()->translate(vec3(0, 4, 0));
       the_camera->set_far_plane(10000);
 
       inputs.init(this);
-     
+
       material *red = new material(vec4(0, 1, 1, 1));
       mesh_box *box = new mesh_box(vec3(100, 1, 100));
       scene_node *node = new scene_node();
@@ -80,15 +97,17 @@ namespace octet {
       mat4t &camera_to_world = camera_node->access_nodeToParent();
       mouse_look_helper.update(camera_to_world);
 
+      fps_helper.update(player_node, camera_node);
+
       // update matrices. assume 30 fps.
-      app_scene->update(1.0f/30);
+      app_scene->update(1.0f / 30);
 
       inputs.update();
 
       // draw the scene
       app_scene->render((float)vx / vy);
 
-      
+
     }
   };
 }
